04a59fcc544bb3fc0d8a96957b2597ed5a060dc7
licongwell <licong@essence.com.cn>
2020-06-01 16:03:00 +0800

    测试文案

--- src/sentry_dingding/forms.py
+++ src/sentry_dingding/forms.py
@@ -6,5 +6,5 @@ from django import forms
 class DingDingOptionsForm(forms.Form):
     access_token = forms.CharField(
         max_length=255,
-        help_text='DingTalk robot access_token'
+        help_text='DingTalk robot access_token-test'
     )
