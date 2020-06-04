52fe76f130d247d50c9093926697b9e7ecb3fe2a
licongwell <licong@essence.com.cn>
2020-06-04 19:42:02 +0800

    markdown测试

--- src/sentry_dingding/plugin.py
+++ src/sentry_dingding/plugin.py
@@ -52,23 +52,22 @@ class DingDingPlugin(NotificationPlugin):
             return
 
         access_token = self.get_option('access_token', group.project)
-        ignore_regular = self.get_option('ignore_regular', group.project)
-        highest_level_regular = self.get_option('highest_level_regular', group.project)
-        medium_level_regular = self.get_option('medium_level_regular', group.project)
-        low_level_regular = self.get_option('low_level_regular', group.project)
-        logging.warn('-----fengexin------')
-        logging.warn(ignore_regular)
-        logging.warn(highest_level_regular)
-        logging.warn(medium_level_regular)
-        logging.warn(low_level_regular)
-        logging.warn('-----fengexin------')
+
+        # ignore_regular = self.get_option('ignore_regular', group.project)
+        # highest_level_regular = self.get_option('highest_level_regular', group.project)
+        # medium_level_regular = self.get_option('medium_level_regular', group.project)
+        # low_level_regular = self.get_option('low_level_regular', group.project)
+
         send_url = DingTalk_API.format(token=access_token)
-        title = u"New alert from {}".format(event.project.slug)
+
+
+
+        title = u"**【高危】错误信息预警**来自 {}".format(event.project.slug)
 
         data = {
             "msgtype": "markdown",
             "markdown": {
-                "title": title,
+                "title": "请注意！！",
                 "text": u"#### {title} \n > {message} [href]({url})".format(
                     title=title,
                     message=event.message,
